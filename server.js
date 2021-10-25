6eea758e5b755cae5d1370d87ab0d1cd13e9f7c7
Tobbelitoo <tobbelotarev@hotmail.com>
2021-10-25 23:12:03 +0200

    first try to view something

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-
+app.get('/', (req, res) => {
+    res.send('App up and running!')
+});
 
 
 app.listen(process.env.PORT || 3000, () => console.log(' Connected to Server'));
